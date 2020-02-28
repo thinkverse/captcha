19534d224c98ccd4e8cd449550d0967e5f7711f9
thinkverse <ctuvedsson@gmail.com>
2020-02-28 01:20:25 +0100

    Remove redundant PlayerManager#get

--- src/main/java/com/github/thinkverse/captcha/managers/PlayerManager.java
+++ src/main/java/com/github/thinkverse/captcha/managers/PlayerManager.java
@@ -1,8 +1,5 @@
 package com.github.thinkverse.captcha.managers;
 
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
-
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
@@ -13,10 +10,6 @@ public final class PlayerManager {
     private Set<UUID> players = new HashSet<>();
 
     private PlayerManager() { }
-
-    public Player get(final UUID uuid) {
-        return players.stream().filter(_uuid -> _uuid.equals(uuid)).map(Bukkit::getPlayer).findAny().orElse(null);
-    }
 
     public boolean contains(final UUID uuid) {
         return this.players.contains(uuid);
